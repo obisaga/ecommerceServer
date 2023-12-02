afaae046048d9da21c0df3ae8dcacebefd4d63f7
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-12-02 17:15:31 +0100

    chnaged cors origin from 5173 to *

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"],
